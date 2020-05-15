1188b206bb66954e09e65fd29edee4eda13f7ba5
Armand Naessens <armandnaessens@Armands-MacBook-Air.local>
2020-05-15 13:56:19 +0200

    dashboard

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required, permission_required
 from . import views
-from .views import home
+from .views import home, dashboard
 
 urlpatterns = [
     path('', home, name='home'),
+    path('dashboard', dashboard, name='dashboard')
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'main/home.html')
+
+def dashboard(request):
+    return render(request, 'main/dashboard.html')
